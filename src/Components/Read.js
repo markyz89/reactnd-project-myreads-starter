ff22c7db265a89e1c487674325bec8b05a05ed9e
Mark Simpson <markyz1989@gmail.com>
2018-07-09 10:42:50 +0100

    Main page static HTML recreated as components

---
+++ src/Components/Read.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+
+function Read (props) {
+	return (
+		 <div className="bookshelf">
+	          <h2 className="bookshelf-title">Read</h2>
+		           <div className="bookshelf-books">
+		            <ol className="books-grid">
+						{props.list.map((book) => (
+							<li key={book.title}>
+								<div className="book">
+									<div className="book-top">
+										<div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${book.img})` }}></div>
+										<div className="book-shelf-changer">
+											<select>
+				                                <option value="move" disabled>Move to...</option>
+				                                <option value="currentlyReading">Currently Reading</option>
+				                                <option value="wantToRead">Want to Read</option>
+				                                <option value="read">Read</option>
+				                                <option value="none">None</option>
+				                            </select>
+										</div> 
+									</div>
+								</div>
+
+								<div className="book-title">{book.title}</div>
+                          		<div className="book-authors">{book.author}</div>
+									
+							</li>
+							))}
+		              </ol>
+					</div>
+		 </div>
+
+
+
+
+		)
+
+
+
+
+
+}
+
+export default Read
